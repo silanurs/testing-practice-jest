5478806a25ca086e1afb655fb83b6857f813e61b
silanurs <sarii.silanur@gmail.com>
2023-06-27 21:24:04 +0300

    write the test for analyzeArray function

---
+++ tests/analyzeArray.test.js
@@ -0,0 +1,7 @@
+const analyzedArray = require('../analyzeArray');
+test('analyzeArray', ()=> {expect(analyzedArray([1,8,3,4,2,6])).toStrictEqual( {
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6
+  })})
